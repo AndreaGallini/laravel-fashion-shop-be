1f524934d8311fb814aca5d419630a66f88b2ef4
daniel iabanji <daniel.iabanji@yahoo.com>
2023-01-23 15:12:26 +0100

    add api

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
+use App\Models\Lead;
+
+class LeadController extends Controller
+{
+//
+}
---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $product = Product::all();
+        return response()->json([
+            'success' => true,
+            'results' => $product
+        ]);
+    }
+
+    public function show($slug)
+    {
+        $product = Product::where('slug', $slug)->with('category', 'tags')->first();
+        if ($product) {
+            return response()->json([
+                'success' => true,
+                'results' => $product
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => 'Nessun prodotto trovato'
+            ]);
+        }
+    }
+}
