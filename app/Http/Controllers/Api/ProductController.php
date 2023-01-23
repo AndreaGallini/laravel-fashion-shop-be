154292e75ca870bbfaf9dc3b32a07af037993a1c
daniel iabanji <daniel.iabanji@yahoo.com>
2023-01-23 15:21:34 +0100

    prova

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -29,7 +29,7 @@ class ProductController extends Controller
         } else {
             return response()->json([
                 'success' => false,
-                'results' => 'Nessun prodotto trovato'
+                'results' => 'Nessun prodotto trovatoo'
             ]);
         }
     }
