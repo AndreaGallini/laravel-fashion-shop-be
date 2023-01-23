2712812f4d2cc92e8f9b3ae19b43587327eeb5fc
AndreGallini <andrea.gallini01@libero.it>
2023-01-23 12:10:38 +0100

    fix seeder

--- database/migrations/2023_01_23_092031_create_products_table.php
+++ database/migrations/2023_01_23_092031_create_products_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100)->unique();
-            $table->string('slug', 255);
+            $table->string('slug', 255)->nullable();
             $table->string('image')->nullable();
             $table->text('description')->nullable();
             $table->integer('n_product')->nullable();
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Product;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class ProductSeeder extends Seeder
 {
@@ -20,12 +22,21 @@ class ProductSeeder extends Seeder
         foreach ($products as $product) {
             $newproduct = new Product();
             $newproduct->name = $product['name'];
-            $newproduct->slug = $product['slug'];
-            $newproduct->image = $product['image'];
+            $newproduct->slug = Str::slug($newproduct->name, '-');
+            $newproduct->image = ProductSeeder::storeimage($product['api_featured_image']);
             $newproduct->description = $product['description'];
-            $newproduct->n_product = $product['n_product'];
-            $newproduct->prezzo = $product['prezzo'];
+         //   $newproduct->n_product = $product['n_product'];
+            $newproduct->prezzo = $product['price'];
             $newproduct->save();
         }
+    }
+        public static function storeimage($img){
+        $url = 'https:'.$img;
+        $contents = file_get_contents($url);
+        $temp_name = substr($url, strrpos($url, '/') + 1);
+        $name = substr($temp_name, 0, strpos($temp_name, '?')) .'.jpg';
+        $path = 'images/' . $name;
+        Storage::put('images/'.$name, $contents);
+        return $path;
     }
 }
