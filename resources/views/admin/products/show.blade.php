30e8f2b5c94ee364b5f75aeba43e979ccec8d8bf
AndreGallini <andrea.gallini01@libero.it>
2023-01-23 16:32:32 +0100

    fix show

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -2,5 +2,9 @@
 @section('content')
     <h1>{{ $product->name }}</h1>
     <p>{{ $product->category->name }}</p>
-    <p>{{ $product->texture->name }}</p>
+                            @if ($product->texture)
+                            <td> {{ $product->texture->name }}</td>
+                             @else
+                            <td>texture non attribuita</td>
+                            @endif
 @endsection
