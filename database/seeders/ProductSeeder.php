52337469a0f41cf05e83242f90d62d5e770facbe
GioGreco <grecogiorgio.97@gmail.com>
2023-01-26 11:54:49 +0100

    randminze tags + colors

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use App\Models\Product;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Arr;
 
 class ProductSeeder extends Seeder
 {
@@ -32,11 +33,18 @@ class ProductSeeder extends Seeder
             $newproduct->brand_id = $product['brand_id'];
             $newproduct->save();
 
-            $colors = [4, 5, 13, 16, 32];
-            $tags = [1, 2, 3, 4, 5];
+            $colors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54];
+            $tags = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 
-            $newproduct->colors()->sync($colors);
-            $newproduct->tags()->sync($tags);
+            $totRandColors = rand(1, 6);
+            $totRandTags = rand(1, 4);
+
+            $prodRandColors = Arr::random($colors, $totRandColors);
+            $prodRandTags = Arr::random($tags, $totRandTags);
+
+
+            $newproduct->colors()->sync($prodRandColors);
+            $newproduct->tags()->sync($prodRandTags);
         }
     }
     public static function storeimage($img)
